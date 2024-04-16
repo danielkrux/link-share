2483d38d5714f55809a7e9acea9479cdffec6bf4
danielkrux <danielmartijn@gmail.com>
2024-04-16 14:13:06 +0200

    add toast message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const font = Instrument_Sans({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${font.className} text-darkgray text-base`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
--- components/CopyToClipboardButton.tsx
+++ components/CopyToClipboardButton.tsx
@@ -2,14 +2,19 @@
 
 import React from "react";
 import Button from "./ui/Button";
+import { useToast } from "./ui/use-toast";
 
 export default function CopyToClipboardButton({
   children,
 }: {
   children: string;
 }) {
+  const { toast } = useToast();
+
   function handleClick() {
     navigator.clipboard.writeText(window.location.href);
+
+    toast({ description: "The link has been copied to your clipboard!" });
   }
 
   return <Button onClick={handleClick}>{children}</Button>;
